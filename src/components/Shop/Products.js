5c1c38706581a579e61d57f4a146dff164b7a25f
Prafull Epili <prafullepili@gmail.com>
2023-01-26 09:39:29 +0530

    added cartTotalQuantity funtionality

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "My first book",
+    price: 6,
+    description: 'my first book i ever wrote'
+  },
+  {
+    id: "p2",
+    title: "My second book",
+    price: 12,
+    description: 'my second book i ever wrote'
+  },
+  {
+    id: "p3",
+    title: "My third book",
+    price: 10,
+    description: 'my third book i ever wrote'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />)}
+
       </ul>
     </section>
   );
